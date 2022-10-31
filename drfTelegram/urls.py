2f4e10d997a7dba4bf03cf41da19397fb52f5470
Albert <adalbert123640@gmail.com>
2022-10-31 19:24:09 +0100

    rename again :(

--- drfTelegram/urls.py
+++ drfTelegram/urls.py
@@ -1,4 +1,4 @@
-"""api URL Configuration
+"""drfTelegram URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drfTelegram.api.views import webhookView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("webhook/", webhookView.as_view())
 ]

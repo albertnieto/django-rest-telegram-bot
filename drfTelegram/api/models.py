d487daa8ff6f5cd298d8512c41776a70d4467232
Albert <adalbert123640@gmail.com>
2022-11-02 20:05:42 +0100

    added models

--- drfTelegram/api/models.py
+++ drfTelegram/api/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+from mongoengine import *
 
 # Create your models here.
+class Pole(EmbeddedDocument):
+    type = StringField(max_length=30)
+    telegram_user_id = IntField()
+    points = IntField()
+    date_time = ComplexDateTimeField()
+
+class Coin(EmbeddedDocument):
+    name = StringField(max_length=30)
+    emoji = StringField(max_length=30)
+
+class User(Document):
+    telegram_id = IntField()
+    is_bot = BooleanField()
+    first_name = ListField(StringField(max_length=50))
+    username = ListField(StringField(max_length=50))
+    groups_id = ListField(StringField(max_length=30))
+    poles = ListField(EmbeddedDocumentField(Pole))
+    coins = ListField(EmbeddedDocumentField(Coin))
+
+class Group(Document):
+    telegram_id = IntField()
+    title = ListField(StringField(max_length=50))
+    type = StringField(max_length=30)
